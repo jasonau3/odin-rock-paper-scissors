84adb1cb7669df5a723c4415f87735aeabbcbdd6
Jason <jasonau003@gmail.com>
2022-02-28 16:14:54 -0500

    ui update

--- app.js
+++ app.js
@@ -44,12 +44,64 @@ const showResult = (result) => {
     }
 }
 
-const buttons = document.querySelectorAll('.buttons');
+const randRGB = () => {
+    let r = Math.floor(Math.random() * (255));
+    let g = Math.floor(Math.random() * (255));
+    let b = Math.floor(Math.random() * (255));
+    return [r,g,b]
+}
 
-const results = document.createElement('div');
-document.body.appendChild(results)
+const endGame = () => {
+    buttons.forEach((button) => {
+        button.disabled = true;
+        button.style.backgroundColor = "#cccccc";
+        button.style.color = "#666666";
+    })
+
+    const playAgain = document.createElement('button');
+    playAgain.textContent = 'Play again?';
+    playAgain.classList.add('playAgain');
+    container.appendChild(playAgain);
+
+    playAgain.addEventListener('click', () => {
+        location.reload()
+    })
+}
+
+const spicePage = () => {
+    container.style.borderRadius = "9px";
+    document.body.style.backgroundColor = 'rgb(' + randRGB().join() + ')';
+
+    let containerRGB = randRGB()
+    container.style.backgroundColor = 'rgb(' + containerRGB.join() + ')';
+
+    if ((containerRGB[0] + containerRGB[1] + containerRGB[2]) < 280) {
+        header.style.color = "#FFF"
+    } else {
+        header.style.color = "#202020"
+    }
+
+    header.style.fontFamily = "Comic Sans MS, Comic Sans, cursive";
+    header.textContent = "Very Fun Rock Paper Scissors"
+    buttons.forEach((button) => {
+        button.classList.add("fun-buttons");
+        button.style.backgroundColor = 'rgb(' + randRGB().join() + ')';
+    })
+
+}
+
+const buttons = document.querySelectorAll('.buttons');
+const container = document.getElementById("container");
+const header = document.getElementsByClassName("heading")[0];
 
 let resultCounter = [0,0]
+
+const results = document.createElement('div');
+results.style.margin = "20px 0";
+results.textContent = `Scores: ${resultCounter[0]} | ${resultCounter[1]}`
+results.style.fontFamily = "Comic Sans MS, Comic Sans, cursive";
+results.style.textAlign = "center";
+container.appendChild(results)
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -59,12 +111,17 @@ buttons.forEach((button) => {
         let computerSelection = computerPlay();
         playRound(playerSelection, computerSelection)
 
+        // spice things up :P
+        spicePage();
+
         // find winner
         if (resultCounter[0] == 5) {
             results.textContent = "You got 5 wins! Good game!"
+            endGame();
         }
         else if (resultCounter[1] == 5) {
             results.textContent = "Computer got 5 wins!! >:( Good game!"
+            endGame();
         }
 
     })
